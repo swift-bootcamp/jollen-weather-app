0374966cafb4e693a3d2c10f6c4e23d0e5059ad3
Jollen Chen <jollen@moko365.com>
2014-08-16 16:02:59 +0800

    invoke REST API

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NSURLConnectionDelegate {
     
-    @IBOutlet var city: UILabel
-    @IBOutlet var icon: UIImageView
-                            
+    @IBOutlet var city: UILabel!
+    @IBOutlet var icon: UIImageView!
+    
+    // 使用 NSMutableData 儲存下載資料
+    var data: NSMutableData = NSMutableData()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,6 +26,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.city.text = "Taipei"
         self.icon.image = UIImage(named: "rainy")
+        
+        startConnection()
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,6 +35,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func startConnection() {
+        var restAPI: String = "http://api.openweathermap.org/data/2.5/weather?q=Taipei"
+        var url: NSURL = NSURL(string: restAPI)
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: true)
+        
+        println("start download")
+    }
+    
+    // 下載中
+    func connection(connection: NSURLConnection!, didReceiveData dataReceived: NSData!) {
+        println("downloading")
+        
+        self.data.appendData(dataReceived)
+    }
+    
+    // 下載完成
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        println("download finished")
+        
+        var json = NSString(data: data, encoding: NSUTF8StringEncoding)
 
+        println(json)
+    }
 }
 
